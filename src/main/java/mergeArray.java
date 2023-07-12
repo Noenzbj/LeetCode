2b53fd54695e11a7a4ef84caac86bb4752c75182
Artur <noenzbj1@mailbox.org>
2023-07-12 09:57:17 +0200

    first commit

---
+++ src/main/java/mergeArray.java
@@ -0,0 +1,14 @@
+public class mergeArray {
+  public static void main(String[] args) {
+    int [] nums1 = {1,2,3};
+    int[] nums2 =  {2,5,6};
+    int m = nums1.length;
+    int n = nums2.length;
+
+    merge(nums1,m,nums2,n);
+  }
+
+  public static void merge(int[] nums1,int m, int [] nums2, int n){
+    System.out.println("test");
+  }
+}
