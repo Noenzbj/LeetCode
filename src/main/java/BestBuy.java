5727f8d36b25b8b333bd04cfc79b9f3453cc8e5e
Artur <noenzbj1@mailbox.org>
2023-08-19 10:46:51 +0200

    Problem solved with faster algorithm

--- src/main/java/BestBuy.java
+++ src/main/java/BestBuy.java
@@ -1,19 +1,39 @@
 public class BestBuy {
   public static void main(String[] args) {
-    int[] prices = {4,3,2,1};
-    maxProfit(prices);
+    int[] prices = {7,1,5,3,6,4};
+    System.out.println(maxProfit(prices));
+
   }
-  public static void maxProfit(int[] prices){
-    int profit=0;
-    int pointer = 0;
-    while (pointer < prices.length) {
-      for (int i = pointer + 1; i < prices.length; i++) {
-        if (prices[i] - prices[pointer] > profit) {
-          profit = prices[i] - prices[pointer];
-        }
+  public static int maxProfit(int[] prices){
+//    int profit=0;
+//    int pointer = 0;
+//    int lowest = 0;
+//    while (pointer < prices.length) {
+//      if (prices[pointer] < prices[lowest] || pointer == 0) {
+//        lowest = pointer;
+//        for (int i = pointer + 1; i < prices.length; i++) {
+//          if (prices[i] - prices[pointer] > profit) {
+//            profit = prices[i] - prices[pointer];
+//          }
+//        }
+//      }
+//
+//      pointer++;
+//    }
+//    return profit;
+
+
+  int profit=0;
+  int lowest = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+      if (prices[i] < prices[lowest]) {
+        lowest = i;
       }
-      pointer++;
+      if (  prices[i]- prices[lowest]>= profit) {
+        profit = prices[i]- prices[lowest];
+      }
     }
-    System.out.println(profit);
+  return profit;
   }
 }
